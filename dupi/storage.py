548c66f08688908fac13f11de0b09c2b925e077b
Jeff Leverenz <jeff.leverenz@gmail.com>
2017-03-13 23:07:49 -0500

    Rename internal hash_dict to path_dict

--- dupi/storage.py
+++ dupi/storage.py
@@ -6,37 +6,37 @@ class Storage:
 
     def __init__(self, filename):
         self.filename = filename
-        self.hash_dict = dict()
+        self.path_dict = dict()
         if os.path.exists(filename):
             with open(filename) as f:
                 json_text = f.read()
                 if json_text != "":
-                    self.hash_dict = json.loads(json_text)
+                    self.path_dict = json.loads(json_text)
 
     def get(self, path):
-        if path in self.hash_dict:
-            return self.hash_dict[path]
+        if path in self.path_dict:
+            return self.path_dict[path]
         else:
             return None
 
     def update(self, stats):
         fullpath = stats['fullpath']
-        self.hash_dict[fullpath] = stats
+        self.path_dict[fullpath] = stats
 
     def insert(self, stats):
         self.update(stats)
 
     def all(self):
-        return self.hash_dict.values()
+        return self.path_dict.values()
 
     def save(self):
         with open(self.filename, "w") as f:
-            f.write(json.dumps(self.hash_dict))
+            f.write(json.dumps(self.path_dict))
 
     def remove(self, filepath):
-        if filepath in self.hash_dict:
-            del self.hash_dict[filepath]
+        if filepath in self.path_dict:
+            del self.path_dict[filepath]
 
     def purge(self):
         open(self.filename, 'w').close()
-        self.hash_dict = dict()
+        self.path_dict = dict()
